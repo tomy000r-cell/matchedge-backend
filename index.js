b594fc71033fefb9f4add81f677fcdf3f7fc780f
tomy000r-cell <to.my000r@gmail.com>
2026-02-14 16:58:00 +0100

    index.js

--- index.js
+++ index.js
@@ -4,21 +4,30 @@ const TelegramBot = require("node-telegram-bot-api");
 const token = process.env.BOT_TOKEN;
 const url = process.env.RENDER_EXTERNAL_URL;
 
+if (!token) {
+  console.error("BOT_TOKEN manquant !");
+  process.exit(1);
+}
+
+if (!url) {
+  console.error("RENDER_EXTERNAL_URL manquant !");
+  process.exit(1);
+}
+
 const app = express();
 app.use(express.json());
 
 const bot = new TelegramBot(token);
 
-// Webhook route
+// Route webhook
 app.post(/bot${token}, (req, res) => {
   bot.processUpdate(req.body);
   res.sendStatus(200);
 });
 
-// Set webhook
+// Activation webhook
 bot.setWebHook(${url}/bot${token});
 
-// Commandes
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, "🔥 MatchEdge Bot actif !");
 });
